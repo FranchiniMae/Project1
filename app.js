999ae4e71459374f46dc32ab43b66b013645d4b6
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-02 22:20:49 -0800

    Adds arrow key navigation

--- app.js
+++ app.js
@@ -8,10 +8,43 @@ console.log("linked.");
 
 $(document).ready(function () {
 	console.log("ready!");
-	setTimer();
+	
+	$('#player').click(function(){
+    $('div.selected').removeClass('selected');
+    $(this).addClass('selected');
+
+	});}).keyup(function(e){
+        var div = $('div.selected');
+        // console.log(div);
+        // console.log(e.which);            
+        switch (e.which) {
+    case 37:
+        $(div).stop().animate({
+            left: '-=75'
+        }); //left arrow key
+        break;
+    case 38:
+        $(div).stop().animate({
+            top: '-=75'
+        }); //up arrow key
+        break;
+    case 39:
+        $(div).stop().animate({
+            left: '+=75'
+        }); //right arrow key
+        break;
+    case 40:
+        $(div).stop().animate({
+            top: '+=75'
+        }); //bottom arrow key
+        break;
+//start timer when you start game
+}
 });
 
+//Navigate
 
+//set Timer
 function setTimer() {
 var count = document.getElementById('countdown');
 timeoutfn = function(){
@@ -25,6 +58,16 @@ setTimeout(timeoutfn, 1000);
 }
 
 
+
+
+// // Monster image
+// var monsterReady = false;
+// var monsterImage = new Image();
+// monsterImage.onload = function () {
+// 	monsterReady = true;
+// };
+// monsterImage.src = "images/monster.png";
+// }
 
 // function navigate() {
 // 	if 
