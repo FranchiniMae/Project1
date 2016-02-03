d40a7dd0ca46391fba2bd4a620fd3a0407c66b33
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-02-02 16:29:43 -0800

    Adds timer feature

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 console.log("linked.");
 
+//setBoard by having a canvas pop up
+//grab player element and pin at random spot on canvas
+//grab target element and pin at random spot on canvas
+//create a timer to count down from 45 seconds
+//add keyboard event listener to character
+
+$(document).ready(function () {
+	console.log("ready!");
+	setTimer();
+});
+
+
+function setTimer() {
+var count = document.getElementById('countdown');
+timeoutfn = function(){
+       count.innerHTML = parseInt(count.innerHTML) - 1;
+       timer = setTimeout(timeoutfn, 1000);
+       if (count.innerHTML === "0") {
+       	clearTimeout(timer);
+     }
+};
+setTimeout(timeoutfn, 1000);
+}
+
+
+
+// function navigate() {
+// 	if 
+// }
